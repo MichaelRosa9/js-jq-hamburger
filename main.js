f0b7ed1bb844a5fc341d904cd74c549139f1ed2a
MichaelRosa9 <michaelrosa9@gmail.com>
2021-04-19 16:09:03 +0200

    revome encolosing function

--- main.js
+++ main.js
@@ -1,20 +1,14 @@
 $(document).ready(function(){
 
   var dropdown_menu = $(".hamburger-menu");
-  var menu = menu();
 
-
+  //clicco l'icona del burger menu per mostrare il menu dropdown
+  $(".header-right > a").click(function(){
+    dropdown_menu.show();
+  })
+  //clicco l'icona "X" per far sparire il menu dropdown
+  $(".hamburger-menu > a").click(function(){
+    dropdown_menu.hide();
+  })
   
-  //funzione che attiva e disattiva il menu burger
-  function menu(){
-    //clicco l'icona del burger menu per mostrare il menu dropdown
-    $(".header-right > a").click(function(){
-      dropdown_menu.show();
-    })
-    //clicco l'icona "X" per far sparire il menu dropdown
-    $(".hamburger-menu > a").click(function(){
-      dropdown_menu.hide();
-    })
-    
-  }
 })
